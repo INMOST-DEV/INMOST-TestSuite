5143b02739db62c8af902b87a93d434d00f622f7
Kirill Terekhov <kirill.terehov@gmail.com>
2021-09-20 10:57:58 +0300

    Update test10.cpp

--- poromech_test/test10.cpp
+++ poromech_test/test10.cpp
@@ -171,9 +171,12 @@ void Test10::SetBC(Mesh & m, double T, MarkerType boundary) const
 	TagRealArray tag_BC_mech = m.GetTag("BOUNDARY_CONDITION_ELASTIC");
 	TagBulk      tag_BC_type = m.GetTag("BCFACEDIR");
 	real BCf[3] = {0,1,0};
-	real BCm_bottom[7] = {1,0,0,1,0,0,0};
+	real BCm_bottom[7] = {1,0,1,0,0,0,0};
+	//real BCm_bottom[7] = { 1,0,0,1,0,0,0 };
 	real BCm_side[7] = {1,0,0,1,0,0,0};
-	real BCm_top[7] = {1,0,0,1,0,0,0};
+	real BCm_top[7] = {0,1,0,1,0,0,0};
+	//real BCm_top[7] = { 1,0,0,1,0,0,0 };
+	//real BCm_top[7] = { 1,0,1,0,0,0,0 };
 	rMatrix mBCf(BCf, 3,1);
 	rMatrix mBCm_bottom(BCm_bottom, 7,1);
 	rMatrix mBCm_side(BCm_side, 7,1);
@@ -259,7 +262,7 @@ void Test10::SetProperty(Mesh & m) const
 #pragma omp parallel
 #endif
 	{
-		rMatrix C(6,6);
+		rMatrix C(6, 6, 0.0);
 #if defined(USE_OMP)
 #pragma omp for
 #endif
@@ -270,6 +273,7 @@ void Test10::SetProperty(Mesh & m) const
 			c.Barycenter(cnt);
 			//rMatrix U,S,V;
 			//perm(c,3,3).SVD(U,S,V);
+			
 			double S0 = perm(c,3,3)(0,0)*c.Volume();
 			double S1 = perm(c,3,3)(1,1)*c.Volume();
 			double S2 = perm(c,3,3)(2,2)*c.Volume();
@@ -296,6 +300,12 @@ void Test10::SetProperty(Mesh & m) const
 			S1 /= tot_vol;
 			S2 /= tot_vol;
 			
+			/*
+			double S0 = perm(c, 3, 3)(0, 0);
+			double S1 = perm(c, 3, 3)(1, 1);
+			double S2 = perm(c, 3, 3)(2, 2);
+			double avg_poro = poro[c];
+			*/
 			//tag_B(c,3,3) = perm(c,3,3)*0.0001;
 			//tag_B(c,3,3) = rMatrix::Unit(3);
 			//tag_B(c,3,3) = rMatrix::Unit(3)*avg_poro;
@@ -305,81 +315,201 @@ void Test10::SetProperty(Mesh & m) const
 			//tag_B(c,3,3) = rMatrix::Unit(3);
 			tag_B(c,3,3).CheckNans();
 			
-			real E1,E2,E3,nu21,nu12,nu31,nu13,nu32,nu23,G23,G13,G12;
+			//real E1,E2,E3,nu12,nu13,nu23,G23,G13,G12;
 			
 			
+			// transversal isotropy:
+			// free: E1, E3, nu12, nu13, G13
+			// E2 = E1
+			// nu23 = nu13
+			// G13 = G23
+			// G12 = 0.5*E1/(1+nu12)
+
+			/*
+			E1 = 0.1 + S0*100.0;
+			E2 = 0.1 + S1*100.0;
+			E3 = 0.1 + S2*100.0;
+			nu12 = 0.15;
+			nu13 = nu23 = 0.05;
+			G23 = 1 + S0*150.0;
+			G13 = 1 + S1*150.0;
+			//if( E1 == E2 && nu13 == nu23 )
+			//	G12 = 0.5 * E1 / (1 + nu12);
+			//else
+			G12 = 1 + S2*150.0;
+			*/
+			/*
+			//works
+			E1 = S0*400;
+			E2 = S1*400;
+			E3 = S2*400;
+			G23 = 300 * S0;
+			G13 = 300 * S1;
+			G12 = 300 * S2;
+			nu12 = 0.1 / E2 * E1;
+			nu13 = 0.1 / E3 * E1;
+			nu23 = 0.2 / E3 * E2;
+			*/
+
+			/*
+			E1 = S0 * 1000;
+			E2 = S1 * 1000;
+			E3 = S2 * 1000;
+			G23 = 500 * S0;
+			G13 = 500 * S1;
+			G12 = 500 * S2;
+			nu12 = 0.1 / E2 * E1;
+			nu13 = 0.1 / E3 * E1;
+			nu23 = 0.2 / E3 * E2;
 			
+			//C.Print();
+			if (tag_E[c].size() == 21)
+			{
+				raSymmetricMatrix E = raSymmetricMatrixMake(tag_E[c].data(), 6);
+				E = GenAnisotropicTensor(E1, E2, E3, nu12, nu13, nu23, G12, G13, G23);
+				E.CheckNans();
+
+				if (!rCheckEigen(E, false))
+				{
+#pragma omp critical
+					{
+						real nu21, nu31, nu32;
+						nu21 = nu12 / E1 * E2;
+						nu31 = nu13 / E1 * E3;
+						nu32 = nu23 / E2 * E3;
+						std::cout << "K:" << std::endl;
+						perm(c, 3, 3).Print();
+						std::cout << "S: " << S0 << " " << S1 << " " << S2 << std::endl;
+						
+						std::cout << "E1: " << E1 << "; E2: " << E2 << "; E3: " << E3 << ";" << std::endl;
+						std::cout << "nu12: " << nu12 << "; nu13: " << nu13 << "; nu23: nu13; " << std::endl;// << nu23 << std::endl;
+						//std::cout << "nu21: " << nu21 << " nu31: " << nu31 << " nu32: " << nu32 << std::endl;
+						std::cout << "G12: " << G12 << "; G13: " << G13 << "; G23: " << G23 << "; " << std::endl;
+
+						std::cout << "E:" << std::endl;
+						E.Print();
+
+						std::cout << "negative!" << std::endl;
+						rCheckEigen(tag_E(c, 6, 6));
+					}
+
+				}
+			}
+			else if (tag_E[c].size() == 36)
+			{
+				tag_E(c, 6, 6) = GenAnisotropicTensor(E1, E2, E3, nu12, nu13, nu23, G12, G13, G23);
+				tag_E(c, 6, 6).CheckNans();
+
+				if (!rCheckEigen(tag_E(c, 6, 6), false))
+				{
+					std::cout << "K:" << std::endl;
+					perm(c, 3, 3).Print();
+					std::cout << "S: " << S0 << " " << S1 << " " << S2 << std::endl;
+					
+
+
+					std::cout << "E:" << std::endl;
+					tag_E(c, 6, 6).Print();
+
+					std::cout << "negative!" << std::endl;
+					rCheckEigen(tag_E(c, 6, 6));
+
+				}
+			}
+			else std::cout << __FILE__ << ":" << __LINE__ << " do not know what to do with elasticity tensor of size " << tag_E.GetSize() << std::endl;
+			*/
+
+			real E1, E2, E3, nu21, nu12, nu31, nu13, nu32, nu23, G23, G13, G12;
+
+
+
 			E1 = S0;
 			E2 = S1;
 			E3 = S2;
 			nu21 = 0.1;
-			nu12 = nu21*E1/E2;
+			nu12 = nu21 * E1 / E2;
 			nu31 = 0.1;
-			nu13 = nu31*E1/E3;
+			nu13 = nu31 * E1 / E3;
 			nu32 = 0.2;
-			nu23 = nu32*E2/E3;
-			G23 = 0.5*S0;
-			G13 = 0.5*S1;
-			G12 = 0.5*S2;
-			C(0,0) =   1.0/E1;
-			C(0,1) = -nu21/E2;
-			C(0,2) = -nu31/E3;
-			C(1,0) = -nu12/E1;
-			C(1,1) =   1.0/E2;
-			C(1,2) = -nu32/E3;
-			C(2,0) = -nu13/E1;
-			C(2,1) = -nu23/E2;
-			C(2,2) =   1.0/E3;
-			C(3,3) = 1.0/(2.0*G23);
-			C(4,4) = 1.0/(2.0*G13);
-			C(5,5) = 1.0/(2.0*G12);
-			
+			nu23 = nu32 * E2 / E3;
+			G23 = 0.5 * S0;
+			G13 = 0.5 * S1;
+			G12 = 0.5 * S2;
+			C(0, 0) = 1.0 / E1;
+			C(0, 1) = -nu21 / E2;
+			C(0, 2) = -nu31 / E3;
+			C(1, 0) = -nu12 / E1;
+			C(1, 1) = 1.0 / E2;
+			C(1, 2) = -nu32 / E3;
+			C(2, 0) = -nu13 / E1;
+			C(2, 1) = -nu23 / E2;
+			C(2, 2) = 1.0 / E3;
+			C(3, 3) = 1.0 / (2.0 * G23);
+			C(4, 4) = 1.0 / (2.0 * G13);
+			C(5, 5) = 1.0 / (2.0 * G12);
+
 			//C.Print();
-			if( tag_E[c].size() == 21 )
+			if (tag_E[c].size() == 21)
 			{
-				raSymmetricMatrix E = raSymmetricMatrixMake(tag_E[c].data(),6);
+				raSymmetricMatrix E = raSymmetricMatrixMake(tag_E[c].data(), 6);
 				E = C.Invert() * 1000;
 				E.CheckNans();
 			}
-			else if( tag_E[c].size() == 36 )
+			else if (tag_E[c].size() == 36)
 			{
-				tag_E(c,6,6) = C.Invert() * 1000;
-				tag_E(c,6,6).CheckNans();
-				
-				if( !rCheckEigen(tag_E(c,6,6),false) )
-				{
-					std::cout << "K:" << std::endl;
-					perm(c,3,3).Print();
-					std::cout << "S: " << S0 << " " << S1 << " " << S2 << std::endl;
-					std::cout << "C:" <<std::endl;
-					C.Print();
-					
-					
-					
-					std::cout << "E:" <<std::endl;
-					tag_E(c,6,6).Print();
-					
-					std::cout << "C*E:" << std::endl;
-					(C*tag_E(c,6,6)).Print();
-					std::cout << "negative!" << std::endl;
-					rCheckEigen(tag_E(c,6,6));
-					
-				}
+				tag_E(c, 6, 6) = C.Invert() * 1000;
+				tag_E(c, 6, 6).CheckNans();
 			}
-			else std::cout << __FILE__ << ":" << __LINE__ << " do not know what to do with elasticity tensor of size " << tag_E.GetSize() << std::endl;
-			//tag_E(c,6,6)*= 1000;
-			
-			//tag_E(c,6,6) = rMatrix::Unit(6);
-			
-			
-			
-			
-			
+
+			//tag_M[c] = 0.5/(1.0 + avg_poro);
 			tag_M[c] = 0.5;
 		}
 	}
 	std::cout << "Saving props.vtk" << std::endl;
 	m.Save("props.vtk");
+
+	{
+		rMatrix I(3, 3), E(3, 3), n(3, 1), x(3, 1);
+		I = rMatrix::Unit(3);
+		int bad = 0;
+		for (Mesh::iteratorCell it = m.BeginCell(); it != m.EndCell(); ++it) if (it->GetStatus() != Element::Ghost)
+		{
+			ElementArray<Face> faces = it->getFaces();
+			E.Zero();
+			for (ElementArray<Face>::iterator jt = faces.begin(); jt != faces.end(); ++jt)
+			{
+				jt->OrientedNormal(it->self(), n.data());
+				jt->Barycenter(x.data());
+				E += x * n.Transpose();
+			}
+			E = (E / it->Volume() - I);
+			if (E.FrobeniusNorm() > 1.0e-4 || it->Volume() < 0)
+				bad++;
+		}
+		bad = m.Integrate(bad);
+		if (m.GetProcessorRank() == 0)
+			std::cout << "Cells with bad divergence: " << bad << std::endl;
+
+		int num_outside = 0, num_total = 0, negvol = 0;
+#if defined(USE_OMP)
+#pragma omp parallel for reduction(+:num_outside) reduction(+:num_total) reduction(+:negvol)
+#endif
+		for (int it = 0; it < m.CellLastLocalID(); ++it) if (m.isValidCell(it))
+		{
+			Cell c = m.CellByLocalID(it);
+			if (c.GetStatus() == Element::Ghost) continue;
+			Storage::real cnt[3];
+			c->Barycenter(cnt);
+			if (!c->Inside(cnt)) num_outside++;
+			num_total++;
+			if (c->Volume() < 0) negvol++;
+		}
+		num_outside = m.Integrate(num_outside);
+		num_total = m.Integrate(num_total);
+		negvol = m.Integrate(negvol);
+		if (m.GetProcessorRank() == 0)
+			std::cout << "Barycenter is outside for: " << num_outside << " of " << num_total << " cells, negative volume in " << negvol << " cells " << std::endl;
+	}
 }
 
 
